9a8227047d642d25a2fd3f695e37b7d5021b633c
emircanisanc <emircanisanc@gmail.com>
2022-11-14 10:25:05 +0300

    Creating Health And Movement Component

---
+++ Bolt 2D LittleWars/Assets/Scripts/Components/CHealth.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CHealth : MonoBehaviour
+{
+    [SerializeField] private float maxHealth;
+    private float currentHealth;
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+    }
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+    public float GetHealthPercent()
+    {
+        return currentHealth / maxHealth;
+    }
+    public float ReduceHealth(float delta)
+    {
+        currentHealth -= delta;
+        currentHealth = Mathf.Max(0, currentHealth);
+        return currentHealth;
+    }
+    public bool IsDead()
+    {
+        return currentHealth <= 0;
+    }
+}
---
+++ Bolt 2D LittleWars/Assets/Scripts/Components/CMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float inputAxis;
+    private Vector3 direction;
+
+    private Rigidbody2D rg2D;
+
+    void Awake()
+    {
+        rg2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(inputAxis > 0)
+        {
+            rg2D.MovePosition(transform.position + direction * inputAxis * speed);
+        }    
+    }
+
+    public void AddMovementInput(Vector3 direction, float inputAxis)
+    {
+        this.inputAxis = inputAxis;
+        this.direction = direction;
+    }
+
+}
